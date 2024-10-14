e254a0e97ae58b58f797d11dba3514140753c80f
Pralav55 <pralavphakatkar007@gmail.com>
2024-10-14 23:09:31 +0530

    Typescript_Practice.git

---
+++ Day1-Practice-Functions.js
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+//  Name                    :       Typescript Practice Functions Day - 1 
+//  Date                    :       14th October 2024
+//  Created By              :       Pralav Phakatkar
+//  Number of functions     :       5
+//
+//////////////////////////////////////////////////////////////////////////////////
+//Function to print 5 times "Marvellous " on Screen
+function DisplayMarvellous(iNo) {
+    var icnt = 0;
+    for (icnt = 1; icnt <= iNo; icnt++) {
+        console.log("Marvellous");
+    }
+}
+//Program to print input number to 1 on screen
+function Display(iNo) {
+    var icnt = 0;
+    for (icnt = iNo; icnt > 0; icnt--) {
+        console.log(icnt);
+    }
+}
+////Accept one Number and Check whether it is divisible by 5 or not
+function Divisible(iNo) {
+    if (iNo == 0) {
+        return false;
+    }
+    else if ((iNo % 5) == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+//Accept one number from user and print that number of * on screen
+function Startprint(iNo) {
+    var icnt = 0;
+    for (icnt = 1; icnt <= iNo; icnt++) {
+        console.log("*");
+    }
+}
+Startprint(10);
